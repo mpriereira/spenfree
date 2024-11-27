9e16d06ad452ab06555a798930d82af45c891835
Mario Prieto <mprietopereira@gmail.com>
2024-11-27 10:28:09 +0100

    chore: expense form modal optimizations

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,80 +1,84 @@
 'use client'
 
-import { useEffect, useRef, useState } from 'react'
-import Link from 'next/link'
-import { Expense, Category } from '@prisma/client'
-import { toast } from 'toaster-ts'
-import { getCategories } from '@/app/expenses/actions'
+import { forwardRef } from 'react'
+import { Expense } from '@prisma/client'
+import { useCategories } from '@/hooks/useCategories'
 import styles from './ExpenseForm.module.css'
 
-export const ExpenseForm = ({
-  expense,
-  onSave,
-}: {
-  expense?: Expense
-  onSave: (formData: FormData) => Promise<void>
-}) => {
-  const formRef = useRef<HTMLFormElement>(null)
-  const [categories, setCategories] = useState<Category[]>([])
-
-  useEffect(() => {
-    const fetchCategories = async () => {
-      const categories = await getCategories()
-      setCategories(categories)
-    }
-    fetchCategories()
-  }, [])
-
-  const getFormattedDate = () => {
-    const date = expense?.date ?? new Date()
-    return date.toISOString().split('T')[0]
-  }
-
-  const save = async (formData: FormData) => {
-    onSave(formData)
-      .then(() => {
-        formRef.current?.reset()
-      })
-      .catch(() => {
-        toast.error('Error while saving the expense')
-      })
-  }
-
-  return (
-    <form ref={formRef} action={save} className={styles.form}>
-      <select required name="category" defaultValue={expense?.categoryId ?? ''}>
-        <option value="" disabled>
-          Select category
-        </option>
-        {categories.map((category) => (
-          <option key={category.id} value={category.id}>
-            {category.name}
-          </option>
-        ))}
-      </select>
-      <input
-        required
-        type="text"
-        name="title"
-        placeholder="Title"
-        defaultValue={expense?.title ?? ''}
-      />
-      <input
-        required
-        type="date"
-        name="date"
-        placeholder="Date"
-        defaultValue={getFormattedDate()}
-      />
-      <input
-        required
-        type="number"
-        name="amount"
-        placeholder="Amount"
-        defaultValue={expense?.amount}
-      />
-      <button type="submit">{expense ? 'Edit expense' : 'Add expense'}</button>
-      {expense && <Link href={`/expenses`}>Cancel</Link>}
-    </form>
-  )
+type ExpenseFormProps = {
+  expense: Expense | null
+  onSubmit: (formData: FormData) => Promise<void>
+  onCancel: () => void
 }
+
+export const ExpenseForm = forwardRef<HTMLFormElement, ExpenseFormProps>(
+  (
+    {
+      expense,
+      onSubmit,
+      onCancel,
+    }: {
+      expense: Expense | null
+      onSubmit: (formData: FormData) => Promise<void>
+      onCancel: () => void
+    },
+    ref,
+  ) => {
+    const { categories } = useCategories()
+
+    const getFormattedDate = () => {
+      const date = expense?.date ?? new Date()
+      return date.toISOString().split('T')[0]
+    }
+
+    return (
+      <form ref={ref} action={onSubmit} className={styles.form}>
+        {categories.length > 0 && (
+          <select
+            required
+            name="category"
+            defaultValue={expense?.categoryId ?? ''}
+          >
+            <option value="" disabled>
+              Select category
+            </option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+        )}
+        <input
+          required
+          type="text"
+          name="title"
+          placeholder="Title"
+          defaultValue={expense?.title ?? ''}
+        />
+        <input
+          required
+          type="date"
+          name="date"
+          placeholder="Date"
+          defaultValue={getFormattedDate()}
+        />
+        <input
+          required
+          type="number"
+          name="amount"
+          placeholder="Amount"
+          defaultValue={expense?.amount}
+        />
+        <footer>
+          <button type="submit">
+            {expense ? 'Edit expense' : 'Add expense'}
+          </button>
+          <button onClick={onCancel}>Cancel</button>
+        </footer>
+      </form>
+    )
+  },
+)
+
+ExpenseForm.displayName = 'ExpenseForm'
--- src/components/ExpenseFormModal.tsx
+++ src/components/ExpenseFormModal.tsx
@@ -4,10 +4,9 @@ import { useRef } from 'react'
 import { useRouter } from 'next/navigation'
 import { toast } from 'toaster-ts'
 import { saveExpense } from '@/app/expenses/actions'
-import { useCategories } from '@/hooks/useCategories'
 import { useSelectedExpense } from '@/hooks/useSelectedExpense'
 import { Modal } from '@/components/Modal'
-import styles from '@/components/ExpenseFormModal.module.css'
+import { ExpenseForm } from '@/components/ExpenseForm'
 
 type ExportFormModalProps = {
   isOpen: boolean
@@ -17,16 +16,9 @@ type ExportFormModalProps = {
 export const ExpenseFormModal = ({ isOpen, close }: ExportFormModalProps) => {
   const formRef = useRef<HTMLFormElement>(null)
   const router = useRouter()
-  const { categories } = useCategories()
   const { selectedExpense: expense } = useSelectedExpense()
-  const actionTitle = expense ? 'Edit expense' : 'Add expense'
 
-  const getFormattedDate = () => {
-    const date = expense?.date ?? new Date()
-    return date.toISOString().split('T')[0]
-  }
-
-  const save = async (formData: FormData) => {
+  const handleSubmit = async (formData: FormData) => {
     toast.promise(saveExpense(formData, expense?.id), {
       loading: 'Saving expense...',
       success: () => {
@@ -50,52 +42,17 @@ export const ExpenseFormModal = ({ isOpen, close }: ExportFormModalProps) => {
 
   return (
     <Modal
-      title={actionTitle}
+      title={expense ? 'Edit expense' : 'Add expense'}
       isOpen={isOpen}
       onClose={close}
       hideFooter={true}
     >
-      <form ref={formRef} action={save} className={styles.form}>
-        <select
-          required
-          name="category"
-          defaultValue={expense?.categoryId ?? ''}
-        >
-          <option value="" disabled>
-            Select category
-          </option>
-          {categories.map((category) => (
-            <option key={category.id} value={category.id}>
-              {category.name}
-            </option>
-          ))}
-        </select>
-        <input
-          required
-          type="text"
-          name="title"
-          placeholder="Title"
-          defaultValue={expense?.title ?? ''}
-        />
-        <input
-          required
-          type="date"
-          name="date"
-          placeholder="Date"
-          defaultValue={getFormattedDate()}
-        />
-        <input
-          required
-          type="number"
-          name="amount"
-          placeholder="Amount"
-          defaultValue={expense?.amount}
-        />
-        <footer>
-          <button type="submit">{actionTitle}</button>
-          <button onClick={handleClose}>Cancel</button>
-        </footer>
-      </form>
+      <ExpenseForm
+        expense={expense}
+        ref={formRef}
+        onSubmit={handleSubmit}
+        onCancel={handleClose}
+      />
     </Modal>
   )
 }
