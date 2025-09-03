4474a8644033835ccf68c62917c75e3f2921947b
Mario Prieto <mprietopereira@gmail.com>
2025-09-03 11:47:21 +0200

    Upgrade to Prisma v6

--- src/app/lib/theme.ts
+++ src/app/lib/theme.ts
@@ -2,18 +2,4 @@ import { createTheme } from '@mantine/core'
 
 export const theme = createTheme({
   primaryColor: 'dark',
-  colors: {
-    'ocean-blue': [
-      '#7AD1DD',
-      '#5FCCDB',
-      '#44CADC',
-      '#2AC9DE',
-      '#1AC2D9',
-      '#11B7CD',
-      '#09ADC3',
-      '#0E99AC',
-      '#128797',
-      '#147885',
-    ],
-  },
 })
